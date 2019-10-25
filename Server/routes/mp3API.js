040ccb7aa767921e19cbfd84afe0e0f885286ba9
Kecseti_Istvan <kecse09@gmail.com>
2019-10-25 19:16:35 +0300

    speech recognition works and gives back correct answer in json

--- Server/routes/mp3API.js
+++ Server/routes/mp3API.js
@@ -1,24 +1,82 @@
 const fs = require('fs'),
-  eformidable = require('express-formidable');
+  eformidable = require('express-formidable'),
+  speech = require('@google-cloud/speech');
 const router = require('express').Router();
 
 const uploadDir = ('./uploadDir');
 if (!fs.existsSync(uploadDir)) {
   fs.mkdirSync(uploadDir);
 }
-router.use(eformidable({ uploadDir }));
+router.use(eformidable({ uploadDir, keepExtensions: true }));
 
+function assembelMessage(words) {
+  let warningLevel,
+    animalObserved,
+    voiceToPlay;
 
-router.post('/api/mp3', (req, res) => {
-  const { mp3 } = req.files;
-  fs.rename(mp3.path, `${mp3.path}.mp3`, (err) => { if (err) console.log(err); });
-  const message = {
-    warningLevel: 'High',
-    coordinates: 'Itt ne',
-    animalObserved: 'Medve',
-    voiceToPlay: 'medve.mp3',
+  if (words.includes('medve')) {
+    animalObserved = 'Medve';
+    voiceToPlay = 'MedveWarning.mp3';
+    warningLevel = 10;
+  }
+
+  if (words.includes('farkas')) {
+    animalObserved = 'Farkas';
+    voiceToPlay = 'FarkasWarning.mp3';
+    warningLevel = 8;
+  }
+
+  if (words.includes('kutya')) {
+    animalObserved = 'Kutya';
+    voiceToPlay = 'KutyaWarning.mp3';
+    warningLevel = 6;
+  }
+
+  if (words.includes('zalan')) {
+    animalObserved = 'Zalan';
+    voiceToPlay = 'ZalanWarning.mp3';
+    warningLevel = 0;
+  }
+
+  if (words.includes('sanyika')) {
+    animalObserved = 'Sandor';
+    voiceToPlay = 'SandorWarning.mp3';
+    warningLevel = 0;
+  }
+
+  return {
+    warningLevel,
+    coordinates: '0,0',
+    animalObserved,
+    voiceToPlay,
+    dateTime: new Date(),
   };
-  res.json(message);
+}
+
+router.post('/api/mp3', async (req, res) => {
+  const { mp3 } = req.files;
+
+  const client = new speech.SpeechClient();
+  const fileName = mp3.path;
+  const file = fs.readFileSync(fileName);
+  const audioBytes = file.toString('base64');
+
+  const audio = {
+    content: audioBytes,
+  };
+  const config = {
+    encoding: 'MP3',
+    languageCode: 'hu-HU',
+    sampleRateHertz: 18000,
+  };
+  const request = {
+    audio,
+    config,
+  };
+  const [response] = await client.recognize(request);
+  const words = response.results.map(result => result.alternatives[0].transcript).join('\n');
+  const sendBack = assembelMessage(words);
+  res.json(sendBack);
 });
 
 module.exports = router;
