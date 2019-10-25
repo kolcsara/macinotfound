8d4f12defdec7050b7b9661f5934c428403e97c9
Attila Kolcsar <kolcsaara95@gmail.com>
2019-10-25 15:50:41 -0700

    drop api handler. restart

--- raspberry/RaspBerry/rpi.api/Areas/HelpPage/ModelDescriptions/CollectionModelDescription.cs
+++
@@ -1,7 +0,0 @@
-namespace rpi.api.Areas.HelpPage.ModelDescriptions
-{
-    public class CollectionModelDescription : ModelDescription
-    {
-        public ModelDescription ElementDescription { get; set; }
-    }
-}
--- raspberry/RaspBerry/rpi.api/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
+++
@@ -1,6 +0,0 @@
-namespace rpi.api.Areas.HelpPage.ModelDescriptions
-{
-    public class SimpleTypeModelDescription : ModelDescription
-    {
-    }
-}
