b8058e4acf3054e5171db458a466609c10f55ec3
Kecseti_Istvan <kecse09@gmail.com>
2019-10-25 13:47:35 +0300

    eslint setup

--- Server/app.js
+++ Server/app.js
@@ -1,11 +1,11 @@
-var express = require('express');
-var mp3API = require('./routes/mp3API');
+const express = require('express');
+const mp3API = require('./routes/mp3API');
 
-var app = express();
+const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
 app.use('/', mp3API);
 
-app.listen(5500,() => {console.log('App listens...');});
+app.listen(5500, () => { console.log('App listens...'); });
