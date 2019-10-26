18de4b0c9f2d7eff54753baede1e38c9e571c4eb
Kecseti_Istvan <kecse09@gmail.com>
2019-10-26 20:11:01 +0300

    File to server works

--- SoundLevelChecker/src/Capture.java
+++ SoundLevelChecker/src/Capture.java
@@ -1,13 +1,28 @@
 import java.awt.BorderLayout;
 import java.awt.Container;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.BufferedReader;
 import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.ProtocolException;
+import java.net.URL;
+import java.net.URLConnection;
+import java.net.URLEncoder;
+import java.nio.charset.StandardCharsets;
 import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.sound.sampled.AudioFileFormat;
 import javax.sound.sampled.AudioFormat;
@@ -20,31 +35,30 @@ import javax.sound.sampled.TargetDataLine;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 
+import org.apache.http.*;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.ContentType;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.entity.mime.MultipartEntityBuilder;
+
 public class Capture extends JFrame {
       private ByteArrayOutputStream out;
       private AudioFormat format;
+      List<File> filesToSend = new ArrayList<File>();
       int history[] = new int[100];
       int noiseLevel = 0;
       int lastIndex = 0;
       
       public Capture() {
-        super("Capture Sound Demo");
+        super("Capture Sound Application");
         setDefaultCloseOperation(EXIT_ON_CLOSE);
-        Container content = getContentPane();
-        final JButton capture = new JButton("Capture");
-        capture.setEnabled(true);
-        capture.addActionListener(new ActionListener() {
-			@Override
-			public void actionPerformed(ActionEvent arg0) {
-				capture.setEnabled(false);
-	            try {
-					captureAudio();
-				} catch (LineUnavailableException e) {
-					e.printStackTrace();
-				}
-			}
-        });
-        content.add(capture,BorderLayout.NORTH);
+        try {
+			captureAudio();
+		} catch (LineUnavailableException e) {
+			e.printStackTrace();
+		}
         
         for(int i = 0 ;i < 100; i++) {
         	history[i] = 0;
@@ -67,21 +81,41 @@ public class Capture extends JFrame {
 
           audioInputStream.close();
           out.close();
+          
           out = new ByteArrayOutputStream();
       }
       
       private void logToHistory(int toAdd) {
-    	  if(lastIndex < 99) {
+    	  if(lastIndex < 98) {
     		  lastIndex++;
     		  history[lastIndex]=toAdd;
     	  }else {
-    		  for(int i = 0; i < 99; i++)history[i] = history[i+1];
-    		  history[100] = toAdd;
+    		  for(int i = 0; i < 98; i++)history[i] = history[i+1];
+    		  history[99] = toAdd;
     	  }
     	  
-    	  for(int i = 0; i< 100; i++)
-    		  System.out.print(history[i]+" ");
-    	  System.out.print("\n");
+    	  //for(int i = 0; i< 100; i++)
+    		  //System.out.print(history[i]+" ");
+    	  //System.out.print("\n");
+      }
+      
+      
+      public void send(File toSend) throws IOException {
+    	  CloseableHttpClient httpClient = HttpClients.createDefault();
+    	  HttpPost uploadFile = new HttpPost("http://192.168.0.75:5500/api/mp3");
+    	  MultipartEntityBuilder builder = MultipartEntityBuilder.create();
+    	  builder.addBinaryBody(
+    	      "mp3",
+    	      new FileInputStream(toSend),
+    	      ContentType.APPLICATION_OCTET_STREAM,
+    	      toSend.getName()
+    	  );
+
+    	  HttpEntity multipart = builder.build();
+    	  uploadFile.setEntity(multipart);
+    	  CloseableHttpResponse response = httpClient.execute(uploadFile);
+    	  HttpEntity responseEntity = response.getEntity();
+    	  System.out.println(responseEntity.toString());
       }
       
       
@@ -108,8 +142,12 @@ public class Capture extends JFrame {
                     	out.write(buffer, 0, count);
                     }else if(wasNoisy) {
                     	try {
+                    		out.write(buffer, 0, count);
                     		wasNoisy = false;
-							save(Paths.get("C:\\Users\\kecse\\LogMap\\"+ System.currentTimeMillis() +".wav").toFile());
+                    		File f = Paths.get("\/Users\/attila.kolcsar\/MaciSandbox"+ System.currentTimeMillis() +".wav").toFile();
+                    		filesToSend.add(f);
+							save(f);
+							System.out.println("File saved..."+filesToSend.size());
 						} catch (IOException e) {
 							e.printStackTrace();
 						}
@@ -119,19 +157,43 @@ public class Capture extends JFrame {
             }
           };
           
+          Runnable sender = new Runnable() {
+			@Override
+			public void run() {
+				
+				while(true) {
+					System.out.println(filesToSend.size()+"");
+					if(!filesToSend.isEmpty()) {
+						try {
+							System.out.println("File sent...");
+							send(filesToSend.get(0));
+						} catch (IOException e) {
+							e.printStackTrace();
+						}
+						filesToSend.remove(0);
+					}
+				}
+			}
+          };
+          
+          Thread senderThread = new Thread(sender);
+          senderThread.start();
+          
           Thread captureThread = new Thread(runner);
           captureThread.start();
       }
+      
+      
       private AudioFormat getFormat() {
-        float sampleRate = 8000;
+        float sampleRate = 16000;
         int sampleSizeInBits = 8;
         int channels = 1;
         boolean signed = true;
         boolean bigEndian = true;
         return new AudioFormat(sampleRate, sampleSizeInBits, channels, signed, bigEndian);
       }
-      @SuppressWarnings("deprecation")
+      
     public static void main(String args[]) {
-       new Capture().show();
+       new Capture().setVisible(true);
       }  
 }
