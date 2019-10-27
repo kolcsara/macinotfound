5f186e99776c6f5f2f32d5545227a5319bf86b81
Attila Kolcsar <kolcsaara95@gmail.com>
2019-10-27 03:35:10 -0700

    client done

--- webclient/src/script.js
+++ webclient/src/script.js
@@ -1,9 +1,13 @@
 let dan, center0, map;
 let objectsOnMap = [];
 let keys = [];
+let lon,lat;
+let homeMarker;
+let akt;
+
 function contiuousCall(){
   $.ajax({
-    url: "http://192.168.0.75:5500/api/getLast24",
+    url: "http://192.168.0.75:5500/getLast24",
     success: function(result) {
       result.forEach(element => {
           if(keys.indexOf(element._id) == -1 && element.warningLevel) {
@@ -20,7 +24,7 @@ function contiuousCall(){
               case 'farkas':   { picture = '../resources/wolf2.png'; break; }
               case 'kutya':   { picture = '../resources/dog1.png'; break; }
               case 'pasztorkutya':   { picture = '../resources/dog1.png'; break; }
-              case 'roka':   { picture = '../resources/fox11.png'; break; }
+              case 'roka':   { picture = '../resources/fox1.png'; break; }
             }
             let color = "#0000cc";
             switch(element.warningLevel){
@@ -64,14 +68,16 @@ function contiuousCall(){
   objectsOnMap.forEach((e) => {
     e.setMap(map);
   })
-  setTimeout(contiuousCall,20000);
+  setTimeout(contiuousCall,2000);
 }
 
 function setup(){
+
   $('#current').click(() =>{
     let animal = $('#jajj').val();
     let warning = 0;
-
+    lat = homeMarker.getPosition().lat();
+    lon = homeMarker.getPosition().lng();
     switch($('#jajj').val()){
       case 'medve': { warning = 10; break; }
       case 'vad':   { warning = 6; break; }
@@ -87,53 +93,102 @@ function setup(){
     navigator.geolocation.getCurrentPosition((position) => {
       dataField = {
         warningLevel: warning,
-        coordinates:{
-          Longitude:position.coords.longitude,
-          Latitude: position.coords.latitude,
-        },
+        Longitude: lon,
+        Latitude: lat,
         animalObserved:animal
       };
-      console.log(JSON.stringify(dataField))
+
       $.ajax({
-        type : 'POST',
-        url : 'http://192.168.0.75:5500/api/mobileDanger',
-        dataType : 'json',
-        data : JSON.stringify(dataField)
-      })
-
-/*
-      fetch('http://192.168.0.75:5500/api/mobileDanger',{ 
-        method:'POST', 
-        headers: {'Content-Type' : 'application/json'},
-        body : JSON.stringify(dataField),
-      })
-      .then(response => response.json())
-      */
-
-      
-      // .then((message) => {
-      //     console.log(message);
-      // })
-      // $.post({
-      //    url: "http://192.168.0.75:5500/api/mobileDanger",
-      //    contentType: 'application/json',
-      //    body : JSON.stringify(dataField),
-      //    succes: function()  {console.log("jee");},
-      //    error: function(err){console.log(err);}
-      // });
+        type: "POST",
+        url: 'http://192.168.0.75:5500/mobileDanger',
+        dataType: "json",
+        data: dataField, //JSON.stringify(dataField),
+        cache: false,
+        timeout: 5000,
+        success: function(data) {
+            console.log(data);
+        },
+         error: function(jqXHR, textStatus, errorThrown) {
+             console.log('error ' + textStatus + " " + errorThrown);
+        }
+    });
 
    });
   });
+
+   $('#stat').click(() =>{
+    lat = homeMarker.getPosition().lat();
+    lon = homeMarker.getPosition().lng();
+    const dataField = {
+      lat: lat,
+      lon: lon
+    }
+      $.ajax({
+        type: "POST",
+        url: 'http://192.168.0.75:5500/statistics',
+        dataType: "json",
+        data: dataField, //JSON.stringify(dataField),
+        cache: false,
+        timeout: 5000,
+        success: function(data) {
+          const center0 = {lat:lat, lng: lon};
+          if(akt) akt.setMap(null);
+          akt = new google.maps.Circle({
+            strokeColor: "#ff0000",
+            strokeOpacity: 0.8,
+            strokeWeight: 2,
+            center: center0,
+            radius: 5000
+          });
+          akt.setMap(map);
+          homeMarker.setMap(null);
+          console.log(data);
+          let toPrint = "";
+          data.forEach((element) => {
+            toPrint +=element+"\n";
+          });
+          alert("Az elmult heten a kert teruleten a kovetkezo bejelentesek tortentek:\n" + toPrint);
+        },
+         error: function(jqXHR, textStatus, errorThrown) {
+             console.log('error ' + textStatus + " " + errorThrown);
+        }
+    });
+
+   });
 }
 
 async function initMap() {
   map = new google.maps.Map(document.getElementById('map'), {
     zoom: 10,
-    center: new google.maps.LatLng(46.301184, 25.285427199999997),
+    center: new google.maps.LatLng(46.301184, 25.285427),
     mapTypeId: 'terrain'
   });
+  google.maps.event.addListener(map, "click", function (event) {
+    if(akt) akt.setMap(null);
+    lat = event.latLng.lat();
+    lon = event.latLng.lng();
+    const center0 = {lat: lat, lng: lon};
+    if(homeMarker) homeMarker.setMap(null);
+    homeMarker = new google.maps.Marker({
+      position: center0,
+      draggable: true,
+      animation: google.maps.Animation.DROP,
+    });
+    homeMarker.setMap(map);
+    akt = new google.maps.Circle({
+      strokeColor: "#ffffff",
+      strokeOpacity: 0.8,
+      strokeWeight: 2,
+      fillColor: "#ffffff",
+      fillOpacity: 0.15,
+      center: center0,
+      radius: 3000
+    });
+    akt.setMap(map);
+  }); //end addListener
   contiuousCall();
   let script = document.createElement('script');
   //script.src = 'https://developers.google.com/maps/documentation/javascript/examples/json/earthquake_GeoJSONP.js';
   document.getElementsByTagName('head')[0].appendChild(script);
 }
+
